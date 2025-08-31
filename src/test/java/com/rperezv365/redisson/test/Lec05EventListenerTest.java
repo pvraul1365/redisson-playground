a5b3a16e57f932707534d1d235ea19e860845635
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-08-31 19:29:34 +0200

    48. Deleted Event Listener

--- src/test/java/com/rperezv365/redisson/test/Lec05EventListenerTest.java
+++ src/test/java/com/rperezv365/redisson/test/Lec05EventListenerTest.java
@@ -3,6 +3,7 @@ package com.rperezv365.redisson.test;
 import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.DeletedObjectListener;
 import org.redisson.api.ExpiredObjectListener;
 import org.redisson.api.RBucketReactive;
 import org.redisson.client.codec.StringCodec;
@@ -37,7 +38,25 @@ public class Lec05EventListenerTest extends BaseTest {
         // extends expiration
         sleep(11000);
 
-
     }
 
+    @Test
+    public void deletedEventTest() {
+        RBucketReactive<String> bucket = super.client.getBucket("user:1:name", StringCodec.INSTANCE);
+        Mono<Void> set = bucket.set("sam", Duration.ofSeconds(10));
+        Mono<Void> get = bucket.get()
+                .doOnNext(log::info)
+                .then();
+
+        Mono<Void> event = bucket
+                .addListener((DeletedObjectListener) s -> log.info("Deleted: {}", s))
+                .then();
+
+        StepVerifier.create(set.concatWith(get).concatWith(event))
+                .verifyComplete();
+
+        // extends expiration
+        sleep(11000);
+
+    }
 }
